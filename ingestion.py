b46dbe9b19440cbce076d61aa68c1436fb0d4d0a
vishnuva007 <77874524+vishnuva007@users.noreply.github.com>
2024-09-05 11:02:56 +0530

    Add files via upload

---
+++ ingestion.py
@@ -0,0 +1,35 @@
+from dotenv import load_dotenv
+
+load_dotenv()
+
+from langchain_community.document_loaders import ReadTheDocsLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_pinecone import PineconeVectorStore
+from langchain_openai import OpenAIEmbeddings
+
+from consts import INDEX_NAME
+
+embeddings = OpenAIEmbeddings(model="text-embedding-3-small")
+
+def ingest_docs():
+    loader = ReadTheDocsLoader(
+        path=r"langchain-docs\langchain.readthedocs.io\en\latest", encoding='utf-8'
+    )
+
+    raw_documents = loader.load()
+    print(f"loaded {len(raw_documents)} documents")
+
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=600, chunk_overlap=50)
+    documents = text_splitter.split_documents(raw_documents)
+    for doc in documents:
+        new_url = doc.metadata["source"]
+        new_url = new_url.replace("langchain-docs", "https:/")
+        doc.metadata.update({"source": new_url})
+
+    print(f"Going to add {len(documents)} to Pinecone")
+    PineconeVectorStore.from_documents(documents, embeddings, index_name=INDEX_NAME)
+    print("****Loading to vectorstore done ***")
+
+
+if __name__ == "__main__":
+    ingest_docs()
